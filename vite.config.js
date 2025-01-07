e0c29d15389b5e3ea7899ac05cc8f396f811949b
ziyaDev <125736719+ziyaDev@users.noreply.github.com>
2025-01-07 20:22:02 +0100

    add jsconfig and postcss configuration, update dependencies, and enhance vite aliasing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Derive __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@/config": path.resolve(__dirname, "./src/config"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks"),
+      "@/services": path.resolve(__dirname, "./src/services"),
+    },
+  },
+});
